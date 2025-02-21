398f8844d62a0d3d21e43c81d729c39125c85ae7
Oluwabunmi Fife <oluwabunmifife@gmail.com>
2025-02-21 15:21:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -254,4 +254,4 @@ def download_csv(email_type):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
